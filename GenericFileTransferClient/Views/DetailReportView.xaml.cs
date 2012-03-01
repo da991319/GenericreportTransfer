b7f7484e0e08fba445c68e700be5fb12cb0d7e17
da991319 <castaingregis@yahoo.fr>
2012-03-01 14:42:43 -0700

    created usercontrol and views. need to add logic

---
+++ GenericFileTransferClient/Views/DetailReportView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace GenericFileTransferClient.Views
+{
+    /// <summary>
+    /// Description for DetailReportView.
+    /// </summary>
+    public partial class DetailReportView : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the DetailReportView class.
+        /// </summary>
+        public DetailReportView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GenericFileTransferClient/Views/Home.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace GenericFileTransferClient.Views
+{
+    /// <summary>
+    /// Description for Home.
+    /// </summary>
+    public partial class Home : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the Home class.
+        /// </summary>
+        public Home()
+        {
+            InitializeComponent();
+        }
+    }
+}
